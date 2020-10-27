0442d38187a8f09a7ab58e906805eda1788a4e1b
jrathore85 <rathore85@gmail.com>
2020-10-27 09:37:06 +0530

    Update user.js
    added code for SiteSurvey route

--- myexpapp/routes/user.js
+++ myexpapp/routes/user.js
@@ -17,4 +17,22 @@ router.post('/create', function(req, res, next) {
   });
  res.redirect('/user');  // redirect to user form page after inserting the data
 }); 
+router.post('/sitesurvey', function(req, res, next) {
+
+// store all the SiteSurvey input data
+
+const userDetails=req.body;
+// insert user data into users table
+
+var sql = 'INSERT INTO SiteSurvey SET ?';
+
+db.query(sql, userDetails,function (err, data) {
+
+if (err) throw err;
+
+console.log("sitesurvey data is inserted successfully ");
+res.send({"success":true});    
+});
+
+});
 module.exports = router;
